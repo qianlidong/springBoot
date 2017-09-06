0025995cc51488e3cbdbda0ade2e4650c80db9b9
qianlidong <qianlidongjava@163.com>
2017-09-06 09:59:09 +0800

    解决问题

---
+++ src/main/java/com/qian/common/AppContext.java
@@ -0,0 +1,16 @@
+package com.qian.common;
+
+/**
+ * Created by qld on 2017/9/6.
+ */
+public class AppContext {
+
+    //申明这样一个静态变量存储线程的信息
+    public static  ThreadLocal<String> sLocal=new ThreadLocal<String>();
+
+
+
+
+
+
+}
