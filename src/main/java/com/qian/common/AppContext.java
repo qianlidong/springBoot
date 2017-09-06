792cdaa0a40ba61849cfdc7d6359f99d331cf2be
qianlidong <qianlidongjava@163.com>
2017-09-06 10:25:03 +0800

    又加了一个

--- src/main/java/com/qian/common/AppContext.java
+++ src/main/java/com/qian/common/AppContext.java
@@ -1,5 +1,7 @@
 package com.qian.common;
 
+import java.util.Map;
+
 /**
  * Created by qld on 2017/9/6.
  */
@@ -8,7 +10,8 @@ public class AppContext {
     //申明这样一个静态变量存储线程的信息
     public static  ThreadLocal<String> sLocal=new ThreadLocal<String>();
 
-
+    //不同的参数类型变量，搞起来
+    public static  ThreadLocal<Map<String,Object>> mapLocal=new ThreadLocal<Map<String,Object>>();
 
 
 
